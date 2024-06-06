ce196479d6140e18772eebaf644c667fcb915690
chen <565306120@qq.com>
2024-06-06 17:51:46 +0930

    upgrade middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
-
+export default authMiddleware({
+  publicRoutes: ["/api/webhook", "/api/uploadthing"]
+ 
+});
+ 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+ 
